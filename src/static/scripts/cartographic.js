d7e2d9cff7d8d9ddefd7a85d3e1acaaa8f96ccc2
Tudor Stroescu <tudorstroescu@yahoo.com>
2023-04-09 21:00:52 +0300

    responsive map visualization

--- src/static/scripts/cartographic.js
+++ src/static/scripts/cartographic.js
@@ -1,41 +1,40 @@
+var width = 700,
+  height = 600;
 
-var width = 900,
-    height = 600;
+var svg = d3
+  .select("#map_container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
-var svg = d3.select("#map_container")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+var projection = d3.geo
+  .albers()
+  .center([0, 0])
+  .rotate([-17, 0])
+  .scale(5300)
+  .translate([-width / 5, 4500]);
 
-var projection = d3.geo.albers()
-    .center([0, 0])
-    .rotate([-17, 0])
-    .scale(5300)
-    .translate([-width / 5, 4500]);
-
-var geoPath = d3.geo.path()
-    .projection(projection);
+var geoPath = d3.geo.path().projection(projection);
 
 queue().defer(d3.json, "../static/topojson/romania-counties.json").await(ready);
 
 function ready(error, counties) {
+  var romania = topojson.feature(counties, counties.objects.ROU_adm1).features;
+  console.log(romania);
 
-    var romania = topojson.feature(counties, counties.objects.ROU_adm1).features;
-    console.log(romania);
+  svg
+    .append("g")
+    .selectAll("path")
+    .data(romania)
+    .enter()
+    .append("path")
+    .attr("d", geoPath)
+    .attr("class", "county");
 
-    svg.append("g")
-        .selectAll("path")
-        .data(romania)
-        .enter()
-        .append("path")
-        .attr("d", geoPath)
-        .attr("class", "county");
+  var id = 0;
+  d3.selectAll("path").attr("id", function () {
+    return id++;
+  });
 
-    var id = 0;
-    d3.selectAll("path")
-        .attr('id', function () {
-            return id++;
-        })
-
-    //console.log(romania[8].properties.NAME_1);
+  //console.log(romania[8].properties.NAME_1);
 }
