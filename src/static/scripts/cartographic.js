f68f2be94f3674788e19f13abd63c8cecf3e41ed
stefali1-dev <stefanleustean56@gmail.com>
2023-04-09 15:46:54 +0300

    added cartographic visualization

---
+++ src/static/scripts/cartographic.js
@@ -0,0 +1,37 @@
+      
+var width = 900,
+height = 600;
+
+var svg = d3.select( "#map_container" )
+      .append( "svg" )
+      .attr( "width", width )
+      .attr( "height", height );
+  
+var projection = d3.geo.albers()
+    .center([0, 0])
+    .rotate([-17,0])
+    .scale(5300)
+    .translate([-width/5, 4500]);
+
+var geoPath = d3.geo.path()
+    .projection(projection);
+
+queue()
+    .defer(d3.json, "/src/static/topojson/romania-counties.json")
+    .await(ready);
+  
+function ready(error, counties){
+    
+    var romania = topojson.feature(counties, counties.objects.ROU_adm1).features;
+    console.log(romania);
+
+     svg.append("g")
+        .selectAll("path")
+        .data(romania)
+        .enter()
+        .append("path")
+        .attr( "d", geoPath )
+        .attr("class","county");  
+    
+    console.log(romania[8].properties.NAME_1);
+}
