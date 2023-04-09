9c7b644a9a902507a6d69bb507d5f8050fb61e6b
stefali1-dev <stefanleustean56@gmail.com>
2023-04-09 21:35:01 +0300

    made map interactive

--- src/static/scripts/cartographic.js
+++ src/static/scripts/cartographic.js
@@ -1,6 +1,49 @@
+const data = [
+    ["Alba", 20000],
+    ["Arad", 21000],
+    ["Arges", 22000],
+    ["Bacau", 23000],
+    ["Bistrita-Nasaud", 24000],
+    ["Botosani", 25000],
+    ["Braila", 26000],
+    ["Brasov", 27000],
+    ["Bucuresti", 27500],
+    ["Buzau", 28000],
+    ["Calarasi", 29000],
+    ["Caras-Severin", 30000],
+    ["Cluj", 31000],
+    ["Constanta", 32000],
+    ["Covasna", 33000],
+    ["Dambovita", 34000],
+    ["Dolj", 35000],
+    ["Galati", 36000],
+    ["Giurgiu", 37000],
+    ["Gorj", 38000],
+    ["Harghita", 39000],
+    ["Hunedoara", 40000],
+    ["Ialomita", 41000],
+    ["Iasi", 42000],
+    ["Ilfov", 43000],
+    ["Maramures", 44000],
+    ["Mehedinti", 45000],
+    ["Mures", 46000],
+    ["Neamt", 47000],
+    ["Olt", 48000],
+    ["Prahova", 49000],
+    ["Salaj", 50000],
+    ["Satu Mare", 51000],
+    ["Sibiu", 52000],
+    ["Suceava", 53000],
+    ["Teleorman", 54000],
+    ["Timis", 55000],
+    ["Tulcea", 56000],
+    ["Vaslui", 57000],
+    ["Valcea", 57500],
+    ["Vrancea", 58000]
+];
 
 var width = 900,
-    height = 600;
+    height = 450;
 
 var svg = d3.select("#map_container")
     .append("svg")
@@ -11,7 +54,7 @@ var projection = d3.geo.albers()
     .center([0, 0])
     .rotate([-17, 0])
     .scale(5300)
-    .translate([-width / 5, 4500]);
+    .translate([-width / 5 + 100, 4350]);
 
 var geoPath = d3.geo.path()
     .projection(projection);
@@ -36,6 +79,15 @@ function ready(error, counties) {
         .attr('id', function () {
             return id++;
         })
+        .on("click", function (obj) {
+
+            console.log(obj.properties.ID_1);
+            console.log(obj.properties.NAME_1);
+
+            var paragraph = d3.select("#county_info");
+            paragraph.text("Judetul " + obj.properties.NAME_1 + ": " +
+             data[obj.properties.ID_1-1][1] + " someri");
+        });
 
     //console.log(romania[8].properties.NAME_1);
 }
