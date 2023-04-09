886d4f70bf08899556c210c1aede61735555bf73
stefali1-dev <stefanleustean56@gmail.com>
2023-04-09 16:58:13 +0300

    small change

--- src/static/scripts/cartographic.js
+++ src/static/scripts/cartographic.js
@@ -1,22 +1,30 @@
+      
 var width = 900,
-  height = 600;
+height = 600;
 
-var svg = d3
-  .select("#map_container")
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height);
+var svg = d3.select( "#map_container" )
+      .append( "svg" )
+      .attr( "width", width )
+      .attr( "height", height );
+  
+var projection = d3.geo.albers()
+    .center([0, 0])
+    .rotate([-17,0])
+    .scale(5300)
+    .translate([-width/5, 4500]);
 
-var projection = d3.geo
-  .albers()
-  .center([0, 0])
-  .rotate([-17, 0])
-  .scale(5300)
-  .translate([-width / 5, 4500]);
+var geoPath = d3.geo.path()
+    .projection(projection);
 
-var geoPath = d3.geo.path().projection(projection);
+queue()
+    .defer(d3.json, "/src/static/topojson/romania-counties.json")
+    .await(ready);
+  
+function ready(error, counties){
+    
+    var romania = topojson.feature(counties, counties.objects.ROU_adm1).features;
+    console.log(romania);
 
-<<<<<<< HEAD
      svg.append("g")
         .selectAll("path")
         .data(romania)
@@ -32,22 +40,4 @@ var geoPath = d3.geo.path().projection(projection);
     })
 
     //console.log(romania[8].properties.NAME_1);
-=======
-queue().defer(d3.json, "../static/topojson/romania-counties.json").await(ready);
-
-function ready(error, counties) {
-  var romania = topojson.feature(counties, counties.objects.ROU_adm1).features;
-  console.log(romania);
-
-  svg
-    .append("g")
-    .selectAll("path")
-    .data(romania)
-    .enter()
-    .append("path")
-    .attr("d", geoPath)
-    .attr("class", "county");
-
-  console.log(romania[8].properties.NAME_1);
->>>>>>> 2410a761b069145b1053e84d08129d7b0589a5d3
 }
