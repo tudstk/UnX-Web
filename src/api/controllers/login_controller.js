d9f46789823ed4890990925d612d13e2d7fe0d16
Tudor Stroescu <tudorstroescu@yahoo.com>
2023-06-15 00:30:45 +0300

    added profile link and edit profile forms

---
+++ src/api/controllers/login_controller.js
@@ -0,0 +1,67 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "unx",
+  password: "postgres",
+  port: 5432,
+});
+
+async function handleLogin(req, res, JWT_SECRET) {
+  try {
+    let requestBody = "";
+
+    req.on("data", (chunk) => {
+      requestBody += chunk;
+    });
+
+    req.on("end", async () => {
+      const { email, password } = JSON.parse(requestBody);
+
+      const query = "SELECT * FROM users WHERE email = $1";
+      const result = await pool.query(query, [email]);
+
+      if (result.rows.length === 0) {
+        res.statusCode = 401;
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({ error: "Invalid email or password." }));
+        return;
+      }
+
+      const user = result.rows[0];
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+        res.statusCode = 401;
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({ error: "Invalid email or password." }));
+        return;
+      }
+
+      const token = jwt.sign(
+        { userId: user.id, email: user.email, username: user.username },
+        JWT_SECRET,
+        {
+          expiresIn: "1h",
+        }
+      );
+
+      console.log("Login successful!");
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ token }));
+    });
+  } catch (error) {
+    console.error(error);
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ error: "An error occurred while logging in." }));
+  }
+}
+
+module.exports = {
+  handleLogin,
+};
--- src/api/server.js
+++ src/api/server.js
@@ -20,6 +20,9 @@ const pool = new Pool({
   port: 5432,
 });
 
+const { handleLogin } = require("./controllers/login_controller");
+const { handleRegistration } = require("./controllers/register_controller");
+
 const server = http.createServer((req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
@@ -39,94 +42,12 @@ const server = http.createServer((req, res) => {
   } else if (reqPath === "/register" && reqMethod === "POST") {
     handleRegistration(req, res);
   } else if (reqPath === "/login" && reqMethod === "POST") {
-    handleLogin(req, res);
+    handleLogin(req, res, JWT_SECRET);
   } else {
     res.statusCode = 404;
     res.end("Not found");
   }
 });
-
-async function handleRegistration(req, res) {
-  try {
-    let requestBody = "";
-
-    req.on("data", (chunk) => {
-      requestBody += chunk;
-    });
-
-    req.on("end", async () => {
-      const { email, username, password } = JSON.parse(requestBody);
-      const hashedPassword = await bcrypt.hash(password, 10);
-
-      const query =
-        "INSERT INTO users (email, username, password) VALUES ($1, $2, $3)";
-      await pool.query(query, [email, username, hashedPassword]);
-
-      res.statusCode = 201;
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify({ message: "User registered successfully!" }));
-    });
-  } catch (error) {
-    console.error(error);
-    res.statusCode = 500;
-    res.setHeader("Content-Type", "application/json");
-    res.end(
-      JSON.stringify({ error: "An error occurred while registering the user." })
-    );
-  }
-}
-
-async function handleLogin(req, res) {
-  try {
-    let requestBody = "";
-
-    req.on("data", (chunk) => {
-      requestBody += chunk;
-    });
-
-    req.on("end", async () => {
-      const { email, password } = JSON.parse(requestBody);
-
-      const query = "SELECT * FROM users WHERE email = $1";
-      const result = await pool.query(query, [email]);
-
-      if (result.rows.length === 0) {
-        res.statusCode = 401;
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify({ error: "Invalid email or password." }));
-        return;
-      }
-
-      const user = result.rows[0];
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-
-      if (!isPasswordValid) {
-        res.statusCode = 401;
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify({ error: "Invalid email or password." }));
-        return;
-      }
-
-      const token = jwt.sign(
-        { userId: user.id, email: user.email, username: user.username },
-        JWT_SECRET,
-        {
-          expiresIn: "1h",
-        }
-      );
-
-      console.log("Login successful!");
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify({ token }));
-    });
-  } catch (error) {
-    console.error(error);
-    res.statusCode = 500;
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify({ error: "An error occurred while logging in." }));
-  }
-}
 
 const port = 3000;
 server.listen(port, () => {
