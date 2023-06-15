ac25da6eaa55146ead049e1d622a4208d9239492
Tudor Stroescu <tudorstroescu@yahoo.com>
2023-06-15 12:55:15 +0300

    small clean

--- src/api/server.js
+++ src/api/server.js
@@ -89,33 +89,6 @@ function handleGetAccount(req, res) {
   );
 }
 
-function handleGetAccount(req, res) {
-  const token = req.headers.authorization;
-  const [, payload] = token.split(".");
-  const decodedPayload = Buffer.from(payload, "base64").toString();
-  const { username } = JSON.parse(decodedPayload);
-
-  pool.query(
-    "SELECT first_name, last_name FROM users WHERE username = $1",
-    [username],
-    (error, results) => {
-      if (error) {
-        console.error("Error retrieving account:", error);
-        res.statusCode = 500;
-        res.setHeader("Content-Type", "text/plain");
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.end("Internal Server Error");
-      } else {
-        const accountDetails = results.rows[0];
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.end(JSON.stringify(accountDetails));
-      }
-    }
-  );
-}
-
 function handleUpdateAccount(req, res) {
   let body = [];
   req
