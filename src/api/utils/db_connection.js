ac1bb397b45aa073fe982c4707d6fb907bdb05f0
stefali1-dev <stefanleustean56@gmail.com>
2023-06-19 11:22:28 +0300

    done

--- src/api/utils/db_connection.js
+++ src/api/utils/db_connection.js
@@ -4,8 +4,9 @@ const pool = new Pool({
   user: "postgres",
   host: "localhost",
   database: "unx",
-  password: "postgres",
+  password: "b4",
   port: 5432,
+  max: 50, // Increase the maximum number of connections
 });
 
 exports.pool = pool;
