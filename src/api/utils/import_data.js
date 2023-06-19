6999ec270681b4d00dc834034807e0f620ad4464
stefali1-dev <stefanleustean56@gmail.com>
2023-06-19 11:22:28 +0300

    done

--- src/api/utils/import_data.js
+++ src/api/utils/import_data.js
@@ -1,14 +1,8 @@
 const fs = require("fs");
 const { Pool } = require("pg");
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "unx",
-  password: "postgres",
-  port: 5432,
-});
+const pool = require("./db_connection").pool;
 
-async function insertEducationData(filePath) {
+async function insertEducationData(filePath, monthIndex) {
   try {
     const fileData = await fs.promises.readFile(filePath, "utf-8");
     const rows = fileData.trim().split("\n").slice(1);
@@ -17,11 +11,13 @@ async function insertEducationData(filePath) {
     await client.query("BEGIN");
 
     const insertQuery =
-      "INSERT INTO educatie (JUDET, total, fara_studii, invatamant_primar, invatamant_gimnazial, invatamant_liceal, invatamant_posticeal, invatamant_profesional, invatamant_universitar, month) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)";
+      `INSERT INTO educatie (JUDET, total, fara_studii, invatamant_primar, invatamant_gimnazial, invatamant_liceal, invatamant_posticeal, invatamant_profesional, invatamant_universitar, month) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`;
     const insertPromises = rows.map((row) => {
       const values = row.split(",");
-      return client.query(insertQuery, [...values, null]);
+      const insertValues = [...values, monthIndex];
+      return client.query(insertQuery, insertValues);
     });
+
 
     await Promise.all(insertPromises);
     await client.query("COMMIT");
@@ -32,7 +28,7 @@ async function insertEducationData(filePath) {
   }
 }
 
-async function insertMediuData(filePath) {
+async function insertMediuData(filePath, monthIndex) {
   try {
     const fileData = await fs.promises.readFile(filePath, "utf-8");
     const rows = fileData.trim().split("\n").slice(1);
@@ -44,8 +40,10 @@ async function insertMediuData(filePath) {
   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`;
     const insertPromises = rows.map((row) => {
       const values = row.split(",");
-      return client.query(insertQuery, [...values, null]);
+      const insertValues = [...values, monthIndex];
+      return client.query(insertQuery, insertValues);
     });
+
 
     await Promise.all(insertPromises);
     await client.query("COMMIT");
@@ -56,15 +54,79 @@ async function insertMediuData(filePath) {
   }
 }
 
-async function insertDataFromCSVFiles(fileNames, path) {
-  for (const fileName of fileNames) {
-    const filePath = `../api/utils/${path}/${fileName}`;
-    if (path === "someri_educatie_judet") {
-      await insertEducationData(filePath);
-    } else if (path === "someri_mediu_judet") {
-      await insertMediuData(filePath);
-    } else {
-      console.log("Path not found");
+async function insertRataData(filePath, monthIndex) {
+  try {
+    const fileData = await fs.promises.readFile(filePath, "utf-8");
+    const rows = fileData.trim().split("\n").slice(1);
+
+    const client = await pool.connect();
+    await client.query("BEGIN");
+
+    const insertQuery =
+      "INSERT INTO rate (JUDET, total, total_femei, total_barbati, someri_indemnizati, someri_neindemnizati, rata_somajului, rata_somajului_feminina, rata_somajului_masculina, month) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)";
+    const insertPromises = rows.map((row) => {
+      const values = row.split(",");
+      const insertValues = [...values, monthIndex];
+      return client.query(insertQuery, insertValues);
+    });
+
+
+    await Promise.all(insertPromises);
+    await client.query("COMMIT");
+
+    console.log(`Data inserted from ${filePath}`);
+  } catch (error) {
+    console.error(`Error inserting data from ${filePath}:`, error);
+  }
+}
+
+async function insertVarstaData(filePath, monthIndex) {
+  try {
+    const fileData = await fs.promises.readFile(filePath, "utf-8");
+    const rows = fileData.trim().split("\n").slice(1);
+
+    const client = await pool.connect();
+    await client.query("BEGIN");
+
+    const insertQuery =
+      "INSERT INTO varste (JUDET, total, sub_25, age_25_29, age_30_39, age_40_49, age_50_55, age_over_55, month) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)";
+    const insertPromises = rows.map((row) => {
+      const values = row.split(",");
+      const insertValues = [...values, monthIndex];
+      return client.query(insertQuery, insertValues);
+    });
+
+
+    await Promise.all(insertPromises);
+    await client.query("COMMIT");
+
+    console.log(`Data inserted from ${filePath}`);
+  } catch (error) {
+    console.error(`Error inserting data from ${filePath}:`, error);
+  }
+}
+
+
+async function insertDataFromCSVFiles(fileNamesArray, pathArray) {
+  for (let i = 0; i < 4; i++) {
+    const fileNames = fileNamesArray[i];
+    const path = pathArray[i];
+    let monthIndex = 1;
+    for (const fileName of fileNames) {
+      const filePath = `src/api/utils/${path}/${fileName}`;
+      if (path === "someri_educatie_judet") {
+        await insertEducationData(filePath, monthIndex);
+      } else if (path === "someri_mediu_judet") {
+        await insertMediuData(filePath, monthIndex);
+      } else if (path === "someri_tip_judete") {
+        await insertRataData(filePath, monthIndex);
+      } else if (path === "someri_varsta_judet") {
+        await insertVarstaData(filePath, monthIndex);
+      } else {
+        console.log("Path not found");
+      }
+
+      monthIndex++;
     }
   }
 }
